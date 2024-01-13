bbfa28e3425cc26d30a5defc958f99fb12538845
go2023 <tetyanago2018@gmail.com>
2024-01-12 19:42:19 -0800

    journalentries ordering was adjusted, new journalentry button was added on show view

--- app/controllers/jornalentries_controller.rb
+++ app/controllers/jornalentries_controller.rb
@@ -3,7 +3,7 @@ class JornalentriesController < ApplicationController
 
   # GET /jornalentries or /jornalentries.json
   def index
-    @jornalentries = Jornalentry.order(date: :desc).paginate(page: params[:page], per_page: 10)
+    @jornalentries = Jornalentry.order(date: :desc).order(:tested_at).paginate(page: params[:page], per_page: 10)
   end
 
   # GET /jornalentries/1 or /jornalentries/1.json
