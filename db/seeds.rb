e4032161c979c883546adc2d8fa5785152c5cddb
go2023 <tetyanago2018@gmail.com>
2024-01-12 16:44:45 -0800

    seed.rb was created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+50.times do
+  Jornalentry.create!(
+    date: Faker::Date.backward(days: 30),
+    list_of_food: Faker::Food.dish,
+    glucose_mg_dl: Faker::Number.between(from: 80, to: 180),
+    notes: Faker::Lorem.paragraph,
+    tested_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+    created_at: Faker::Time.backward(days: 14),
+    updated_at: Faker::Time.backward(days: 7)
+  )
+end
+
+puts "Seeding complete!"
