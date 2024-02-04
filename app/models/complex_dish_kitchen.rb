327bcd5b2519cf6e2055fb483dddf76ea679d84d
go2023 <tetyanago2018@gmail.com>
2024-02-03 23:51:11 -0800

    start of total calories of complex dish implementation

--- app/models/complex_dish_kitchen.rb
+++ app/models/complex_dish_kitchen.rb
@@ -6,8 +6,16 @@ class ComplexDishKitchen < ApplicationRecord
   accepts_nested_attributes_for :ingredients, allow_destroy: true, reject_if: :all_blank
   # accepts_nested_attributes_for :tasks, allow_destroy: true, reject_if: :all_blank
 
-  def calculate_calories
-    # Your logic to calculate calories here based on the new structure
+  def total_calories
+    total_calories = 0
+
+    ingredients.each do |ingredient|
+      # Calculate the calories for each ingredient based on weight and calories per 100g
+      calories_for_ingredient = (ingredient.ingredient_weight * ingredient.ingredient_calorie_per_100g) / 100.0
+      total_calories += calories_for_ingredient
+    end
+
+    total_calories.round(2) # Round the total calories to 2 decimal places
   end
 end
 
